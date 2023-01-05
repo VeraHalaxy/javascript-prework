3a04ebd72af46629f85eab0b3f35f5af03ae74a1
Vera Halaktionova <halaktionova.v@gmail.com>
2023-01-06 00:36:52 +0100

    Add functions getMoveName and displayResult

--- js/script.js
+++ js/script.js
@@ -1,34 +1,72 @@
+function getMoveName(argMoveId){
+   if(argMoveId == 1){
+      return 'stone';
+   } else if(argMoveId == 2){
+      return 'paper';
+   } else if(argMoveId == 3){
+      return 'scissors';
+   } else {
+      printMessage('Unknown move ' + argMoveId + '.');
+      return 'unknown move';
+   }
+}
+
+function displayResult(argComputerMove, argPlayerMove){
+   printMessage('I played ' + argComputerMove + ', and you ' + argPlayerMove);
+   if( argComputerMove == 'stone' && argPlayerMove == 'paper'){
+      printMessage('You win!');
+   } else if( argComputerMove == 'paper' && argPlayerMove == 'scissors'){
+      printMessage('You win!');
+   } else if( argComputerMove == 'scissors' && argPlayerMove == 'stone'){
+      printMessage('You win!');
+   } else if( argComputerMove == 'paper' && argPlayerMove == 'stone'){
+      printMessage('You lose!');
+   } else if( argComputerMove == 'scissors' && argPlayerMove == 'paper'){
+      printMessage('You lose!');
+   } else if( argComputerMove == 'stone' && argPlayerMove == 'scissors'){
+      printMessage('You win!');
+   } else if( argComputerMove == 'scissors' && argPlayerMove == 'scissors'){
+      printMessage('Dead Heat');
+   } else if( argComputerMove == 'stone' && argPlayerMove == 'stone'){
+      printMessage('Dead Heat');
+   } else if( argComputerMove == 'paper' && argPlayerMove == 'paper'){
+      printMessage('Dead Heat');
+   } else {
+      printMessage('Unknown Move');
+   }
+}
+
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 
 console.log('The random number is: ' + randomNumber);
 
-let computerMove = 'unknown move';
+let computerMove = getMoveName(randomNumber);
 
-if(randomNumber == 1){
+/*if(randomNumber == 1){
    computerMove = 'stone';
 } else if(randomNumber == 2){
    computerMove = 'paper';
 } else(randomNumber == 3){
    computerMove = 'scissors';
-}
+}*/
 
-printMessage('My move is: ' + computerMove);
+//printMessage('My move is: ' + computerMove);
 
 let playerInput = prompt('Choose your move! 1: stone, 2: paper, 3: scissors.');
 
 console.log('The player typed: ' + playerInput);
 
-let playerMove = 'unknown move';
+let playerMove = getMoveName(randomNumber);
 
-if(playerInput == '1'){
+/*if(playerInput == '1'){
    playerMove = 'stone';
 } else if(randomNumber == '2'){
    playerMove = 'paper';
 } else(randomNumber == '3'){
    playerMove = 'scissors';
-}
+}*/
 
-printMessage('Your move is: ' + playerMove);
+/*printMessage('Your move is: ' + playerMove);
 
 if( computerMove == 'stone' && playerMove == 'paper'){
    printMessage('You win!');
@@ -50,4 +88,4 @@ if( computerMove == 'stone' && playerMove == 'paper'){
    printMessage('You lose!');
 } else {
    printMessage('Unknown Move');
-}
+}*/
