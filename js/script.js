8a9bb6589682bf72e8477cb780a8187c770ca417
Vera Halaktionova <halaktionova.v@gmail.com>
2023-01-05 08:36:03 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,2 +1,16 @@
 let computerMove = `stone`;
 printMessage('I played a' + computerMove + 'If your move is paper, you win!');
+
+let playerMove = `paper`;
+
+let randomFraction = Math.random();
+
+printMessage('The fraction drawn is: ' + randomFraction);
+
+let calculation = randomFraction * 3 + 1;
+
+printMessage('A fraction multiplied by 3 and increased by 1: ' + calculation);
+
+let roundNumber = Math.floor(calculation);
+
+printMessage('The number after rounding down is: ' + roundNumber);
